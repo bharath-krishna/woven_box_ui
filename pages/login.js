c9ba30ec1c0362928eee132e872aef4a73cd1e1b
Bharath Krishna <bharath.chakravarthi@gmail.com>
2021-06-01 23:25:48 +0900

    updated login page removed unwanted components added email

--- pages/login.js
+++ pages/login.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 import FirebaseAuth from "../components/FirebaseAuth";
 import {
   authAction,
@@ -6,9 +6,40 @@ import {
   withAuthUserTokenSSR,
 } from "../utils/NextFirebaseAuth";
 import FullPageLoader from "../components/FullPageLoader";
+import { Container, Grid, makeStyles, Typography } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+  container: {
+    display: "flex",
+    justifyContent: "center",
+    textAlign: "center",
+    paddingTop: 150,
+  },
+}));
 
 function Auth() {
-  return <FirebaseAuth />;
+  const classes = useStyles();
+
+  return (
+    <Fragment>
+      <Container className={classes.container}>
+        <Grid container direction="column">
+          <Grid item>
+            <Typography variant="h3" color="primary">
+              Welcome to Woven Box
+            </Typography>
+            <Typography variant="subtitle1" color="secondary">
+              Use your google account to signin. If its first time signing in
+              then click on "Trouble Signing in" and follow the instructions
+            </Typography>
+          </Grid>
+          <Grid item>
+            <FirebaseAuth />
+          </Grid>
+        </Grid>
+      </Container>
+    </Fragment>
+  );
 }
 
 export const getServerSideProps = withAuthUserTokenSSR({
